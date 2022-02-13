e5c8fd27910716d032f826747c693740fdeeddf3
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2022-02-13 11:46:52 +0600

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Button extends React.Component { 
+    shouldComponentUpdate(nextProps) {
+        const {change: currentChange} = this.props;
+        const {change: nextChange} = nextProps;
+
+        if(currentChange === nextChange) {
+            return false;
+        }
+        return true;
+    }
+    
+    render() {
+        console.log('button component rendered');
+        const{change} = this.props;
+        return (
+          <button type='button' onClick={change}>
+              Click here
+          </button>
+        );
+    }
+}
+
+export default Button;
---
+++ src/components/Clock.js
@@ -0,0 +1,39 @@
+import React from 'react'; 
+import Button from './Button';
+
+class Clock extends React.Component {
+    state = { date: new Date(), locale: 'bn-BD' };
+
+    componentDidMount() {
+        this.clockTimer = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
+    }
+
+    handleClick = () =>{
+        this.setState({
+            locale: 'en-US'
+        });
+    }
+
+    render() {
+        const { date, locale } = this.state;
+        return (
+            <div>
+            <h1 className="heading">
+                <span className="text">{date.toLocaleTimeString(locale)}</span>
+            </h1>
+            <Button change={this.handleClick.bind(this, 'en-us')}>Click here</Button>
+            </div>
+        );
+    }
+}
+export default Clock;
